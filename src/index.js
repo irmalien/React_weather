b4e9c3fc2f375764d4121e1ddfa3c9d058993edf
irmalien <mikus.vanags@gmail.com>
2019-01-20 00:43:45 +0100

    comments init

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 // import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import faker from 'faker';
+import CommentDetail from './CommentDetail' // './' indicates same directory
 
+// function getButtonText(){
+//   return "Click on me!";
+// }
 
 // create a react component
 const App = () => {
   return (
-  <div>
-    <label for="name">Enter name:</label>
-    <input id="name" type="text" />
-    <button style="background-color: blue; color: white;">Submit</button>
-  </div>
+    <div className="ui container comments">
+      <CommentDetail author="Sam" timeAgo="Today at 4:45PM"/>
+      <CommentDetail author="Alex" timeAgo="Today at 2:15AM"/>
+      <CommentDetail author="Jane" timeAgo="Yesterday at 3:33PM"/>
+    </div>
 ); 
 }
 
