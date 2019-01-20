55fcc6ab982462a3d31f6a576f2067d9d2631798
irmalien <mikus.vanags@gmail.com>
2019-01-20 14:57:28 +0100

    finalized

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import faker from 'faker';
+// import faker from 'faker';
 
 const CommentDetail = (props) => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()}/>
+        <img alt="avatar" src={props.avatar}/>
       </a>
       <div className="content">
         <a href="/" className="author">
@@ -15,7 +15,7 @@ const CommentDetail = (props) => {
           <span className="date">{props.timeAgo}</span>
         </div>
         <div className="text">
-          Nice blog post
+          {props.comment}
         </div>
         
       </div>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 // import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import faker from 'faker';
-import CommentDetail from './CommentDetail' // './' indicates same directory
+import faker from 'faker';
+import CommentDetail from './CommentDetail'; // './' indicates same 
+import ApprovalCard from './ApprovalCard';
 
 // function getButtonText(){
 //   return "Click on me!";
@@ -12,9 +13,37 @@ import CommentDetail from './CommentDetail' // './' indicates same directory
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" timeAgo="Today at 4:45PM"/>
-      <CommentDetail author="Alex" timeAgo="Today at 2:15AM"/>
-      <CommentDetail author="Jane" timeAgo="Yesterday at 3:33PM"/>
+    <ApprovalCard>
+      <div>
+        <h4>Warning</h4>
+      Are you sure?
+      </div>
+
+    </ApprovalCard>
+    <ApprovalCard>
+      <CommentDetail 
+        avatar={faker.image.avatar()} 
+        author={faker.internet.userName()} 
+        timeAgo="Today at 2:15AM"
+        comment={faker.lorem.text()}
+      />
+    </ApprovalCard>
+    <ApprovalCard>
+      <CommentDetail 
+        avatar={faker.image.avatar()} 
+        author={faker.internet.userName()}  
+        timeAgo="Today at 2:15AM"
+        comment={faker.lorem.text()}
+      />
+    </ApprovalCard>
+    <ApprovalCard>
+      <CommentDetail 
+        avatar={faker.image.avatar()} 
+        author={faker.internet.userName()}  
+        timeAgo="Yesterday at 3:33PM"
+        comment={faker.lorem.paragraph()}
+      />
+    </ApprovalCard>
     </div>
 ); 
 }
