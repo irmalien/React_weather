90a0e61d31d6f249ea1f36e267556bacf4bbeea4
irmalien <mikus.vanags@gmail.com>
2019-01-21 23:15:27 +0100

    finalized with css etc.

--- src/index.js
+++ src/index.js
@@ -1,55 +1,58 @@
 // import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail'; // './' indicates same 
-import ApprovalCard from './ApprovalCard';
+import SeasonDisplay from './SeasonDisplay'
+import Loader from './Loader'
+// import faker from 'faker';
 
-// function getButtonText(){
-//   return "Click on me!";
-// }
+//create React class-based component
 
-// create a react component
-const App = () => {
-  return (
-    <div className="ui container comments">
-    <ApprovalCard>
-      <div>
-        <h4>Warning</h4>
-      Are you sure?
+
+class App extends React.Component {
+  // constructor(props) {
+  //   super(props); //super refers to parent of class - React.Component
+  //   //this.state is ONLY time we DIRECTLY assign value to STATE
+  //   this.state = {lat: null, errorMessage: ''};
+  // }
+  //alternative to constructor
+  state={lat: null, errorMessage: ''}
+
+
+  componentDidMount(){
+    window.navigator.geolocation.getCurrentPosition(
+      //!!!! ALWAYS update value using setState()
+      (position) => {this.setState({ lat: position.coords.latitude })},
+      (err) => this.setState({ errorMessage: err.message }) 
+    );
+    console.log('my component rendered to screen')
+  }
+
+  componentDidUpdate(){
+    console.log('my component was updated')
+  }
+
+  //helper method
+  renderContent(){
+    if(this.state.errorMessage && !this.state.lat){
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+    if(!this.state.errorMessage && this.state.lat){
+      return <SeasonDisplay lat={this.state.lat}/>
+    }
+      return <Loader message="Please accept location request"/>
+  }
+
+  //React says we have to define render!
+  render() {
+    return(
+      <div className="border red">
+        {this.renderContent()}
       </div>
-
-    </ApprovalCard>
-    <ApprovalCard>
-      <CommentDetail 
-        avatar={faker.image.avatar()} 
-        author={faker.internet.userName()} 
-        timeAgo="Today at 2:15AM"
-        comment={faker.lorem.text()}
-      />
-    </ApprovalCard>
-    <ApprovalCard>
-      <CommentDetail 
-        avatar={faker.image.avatar()} 
-        author={faker.internet.userName()}  
-        timeAgo="Today at 2:15AM"
-        comment={faker.lorem.text()}
-      />
-    </ApprovalCard>
-    <ApprovalCard>
-      <CommentDetail 
-        avatar={faker.image.avatar()} 
-        author={faker.internet.userName()}  
-        timeAgo="Yesterday at 3:33PM"
-        comment={faker.lorem.paragraph()}
-      />
-    </ApprovalCard>
-    </div>
-); 
+    )
+  }
 }
 
 // Take the react component and show on screen
-
 ReactDOM.render( <
   App / > ,
   document.querySelector('#root')
